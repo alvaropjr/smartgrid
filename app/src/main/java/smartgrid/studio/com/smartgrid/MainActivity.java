1044c3d434452a143313389d325ce025bf9658c4
Michael <michael.abr@hotmail.com>
2019-03-10 22:07:50 -0300

    Icones pequenos

--- app/src/main/java/smartgrid/studio/com/smartgrid/MainActivity.java
+++ app/src/main/java/smartgrid/studio/com/smartgrid/MainActivity.java
@@ -1,22 +1,22 @@
 package smartgrid.studio.com.smartgrid;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Button botaoEntrar;
+    private TextView botaoCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(getApplicationContext(), "OnCreate metodo chamado",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
+
         botaoEntrar = (Button) findViewById(R.id.botaoEntrarId);
         botaoEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,41 +25,12 @@ public class MainActivity extends Activity {
             }
         });
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getApplicationContext(), "OnStart metodo chamado",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(getApplicationContext(), "OnRestart metodo chamado",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), "OnResume metodo chamado",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), "OnPause metodo chamado",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), "OnStop metodo chamado",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), "OnDestory metodo chamado",Toast.LENGTH_SHORT).show();
+        botaoCadastro = (TextView) findViewById(R.id.botaoCadastroId);
+        botaoCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CadastroActivity.class));
+            }
+        });
     }
 }
